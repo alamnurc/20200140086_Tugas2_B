7f8ab02ddb6a4a4a68b5e94bf9660f6ca986b0b2
lammn <lammn@MACBOOK-PRO-MJL>
2022-03-25 09:41:34 +0700

    Add project files.

---
+++ 20200140086_Tugas2_B/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20200140086_Tugas2_B
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
